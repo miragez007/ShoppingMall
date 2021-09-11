3c3f7764445c88b396de6873255db4d5024edc1a
kanjiz <kanjizm@gmail.com>
2021-09-11 17:14:50 +0700

    ep 38 end

--- lib/ultility/my_constant.dart
+++ lib/ultility/my_constant.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class MyConstant {
   // Genernal 
   static String appName = 'Miragez Project';
-  static String domain = 'https://b970-49-228-164-184.ngrok.io';
+  static String domain = 'https://0964-49-228-164-184.ngrok.io';
 
   // Route
   static String routeAuthen = '/authen';
